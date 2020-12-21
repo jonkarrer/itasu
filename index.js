38c536535ef2d1169686270043ee07fb6e147c0f
jonkarrer <66031598+jonkarrer@users.noreply.github.com>
2020-12-21 09:29:58 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,262 @@
 let projectArr = [];
+let dayListArr = [];
+let play = this;
 let loadStorage;
+localStorage.clear();
+//Daylist builder
+class Daylist {
+    constructor(title){
+        this.id = dayListArr.length;
+        this.projectTitle = title;
+        this.rightPanel = document.getElementById('rightPanel');
+        this.parentContainer = document.createElement('div');
+        this.parentContainer.id = this.projectTitle;
+        this.parentContainer.style.display ='flex';
+        this.parentContainer.style.justifyContent = 'space-evenly';
+        this.parentContainer.style.height = '100%';
+    }
+    startNewProject() {
+        this.titleCacheDom();
+        this.buildTitleForList();
+        this.rightSideCacheDom();
+        this.buildSideBox();
+        this.addOneCheckbox();
+    }
+    titleCacheDom() {
+      
+        this.dayTab = document.createElement('div');
+        this.dayTab.id = 'dayTab';
+
+        this.title = document.createElement('div');
+        this.title.id = "title";
+        
+        this.heading = document.createElement('h1');
+        this.heading.innerText = 'Today';
+       
+        this.addDelete = document.createElement('div');
+        this.addDelete.id = "addDelete";
+        
+        this.add = document.createElement('div');
+        this.add.id = "add";
+        this.add.addEventListener('click', addNewDay);
+        
+        this.checkBoxParent = document.createElement('div');
+        this.checkBoxParent.id = 'checkBoxParent';
+        
+        this.addToListButt = document.createElement('div');
+        this.addToListButt.id = "addToListButton";
+        
+        this.p7 = document.createElement('p7');
+        this.p7.innerText = '+';
+        
+        this.plus = document.createElement('p7');
+        this.plus.innerText = '+';
+    }
+    buildTitleForList() {
+        this.add.appendChild(this.p7);
+        this.addDelete.appendChild(this.add);
+        this.title.appendChild(this.heading);
+        this.title.appendChild(this.addDelete);
+        this.addToListButt.appendChild(this.plus);
+        this.dayTab.appendChild(this.title);
+        this.dayTab.appendChild(this.checkBoxParent);
+        this.dayTab.appendChild(this.addToListButt);
+        this.parentContainer.appendChild(this.dayTab);
+        this.rightPanel.appendChild(this.parentContainer);
+    }
+    rightSideCacheDom() {
+
+        this.sideBar = document.createElement('div');
+        this.sideBar.id = 'rightSideBar';
+        this.deadLineDate = prompt("Deadline?");
+        
+        this.deadLineBox = document.createElement('div');
+        this.deadLineBox.id = 'deadlineBox';
+
+        this.h1 = document.createElement('h1');
+        this.h1.innerText = "Deadline";
+
+        this.p1 = document.createElement('p1');
+        this.p1.innerText = this.deadLineDate;
+
+        this.notes = document.createElement('div');
+        this.notes.id = 'notes';
+
+        this.textArea = document.createElement('textarea');
+        
+
+        this.remove = document.createElement('div');
+        this.remove.id = 'remove';
+    }
+    buildSideBox() {
+        this.deadLineBox.appendChild(this.h1);
+        this.deadLineBox.appendChild(this.p1);
+        this.notes.appendChild(this.textArea);
+        this.sideBar.appendChild(this.deadLineBox);
+        this.sideBar.appendChild(this.notes);
+        this.sideBar.appendChild(this.remove);
+        this.parentContainer.appendChild(this.sideBar);
+        this.rightPanel.appendChild(this.parentContainer);
+
+    }
+    addOneCheckbox(){
+        this.addToListButt.addEventListener('click', () => {
+            this.checkBox = document.createElement('div');
+            this.checkBox.id = 'checkBox';
+    
+            this.input = document.createElement('input');
+            this.input.type = "checkbox";
+    
+            this.p = document.createElement('p');
+            this.p.innerText = prompt('Name of item?');
+
+            this.checkBox.appendChild(this.input);
+            this.checkBox.appendChild(this.p);
+            this.checkBoxParent.appendChild(this.checkBox);
+        })
+    }
+}
+class Day {
+    constructor() {
+        this.header = prompt("What day?");
+    }
+    init() {
+        this.cacheDom();
+        this.addADay();
+        this.removeMe();
+        this.addOneCheckbox();
+    };
+    cacheDom(){
+        this.dayTab = document.getElementById('dayTab');
+
+        this.xtitle = document.createElement('div');
+        this.xtitle.id = "title";
+        
+        this.heading = document.createElement('h1');
+        this.heading.innerText = this.header;
+       
+        this.addDelete = document.createElement('div');
+        this.addDelete.id = "addDelete";
+        
+        this.delete = document.createElement('div');
+        this.delete.id = "delete";
+        this.delete.innerText = "x";       
+        this.checkBoxParent = document.createElement('div');
+        this.checkBoxParent.id = 'checkBoxParent';
+        
+        this.addToListButt = document.createElement('div');
+        this.addToListButt.id = "addToListButton";
+        
+        this.plus = document.createElement('p7');
+        this.plus.innerText = '+';
+    };
+    addADay(){
+        this.addDelete.appendChild(this.delete);
+        this.xtitle.appendChild(this.heading);
+        this.xtitle.appendChild(this.addDelete);
+        this.addToListButt.appendChild(this.plus);
+        this.dayTab.appendChild(this.xtitle);
+        this.dayTab.appendChild(this.checkBoxParent);
+        this.dayTab.appendChild(this.addToListButt);
+    };
+    removeMe(){
+        this.delete.addEventListener('click', () => {
+            this.xtitle.remove();
+            this.checkBoxParent.remove();
+            this.addToListButt.remove();
+        }); 
+    };
+    addOneCheckbox(){
+        this.addToListButt.addEventListener('click', () => {
+            this.checkBox = document.createElement('div');
+            this.checkBox.id = 'checkBox';
+    
+            this.input = document.createElement('input');
+            this.input.type = "checkbox";
+    
+            this.p = document.createElement('p');
+            this.p.innerText = prompt('Name of item?');
+
+            this.checkBox.appendChild(this.input);
+            this.checkBox.appendChild(this.p);
+            this.checkBoxParent.appendChild(this.checkBox);
+        })
+    };
+}
+function addNewDay() {
+    let newDay = new Day();
+    newDay.init();
+}
+//Project builder
+class Project {
+    constructor (projectTitle) {
+        this.projectTitle = projectTitle;
+        this.id = projectArr.length;
+    }
+    init() {
+        this.cacheDom();
+        this.buildProjectRectangle();
+        this.deleteMe();
+    }
+    cacheDom() {
+        this.projectPanel = document.getElementById('projectTabs');
+        this.blankDiv = document.createElement('div');
+        this.blankDiv.id = this.id;
+        this.titleDiv = document.createElement('div');
+        this.iconDiv = document.createElement('div');
+        this.iconDiv.className = 'gg-trash';
+        this.titleParaElement = document.createElement('p1');   
+    }
+    buildProjectRectangle() {
+        this.titleParaElement.innerText = this.projectTitle;
+        this.titleDiv.appendChild(this.titleParaElement);
+        this.blankDiv.appendChild(this.titleDiv);
+        this.blankDiv.appendChild(this.iconDiv);
+        this.projectPanel.appendChild(this.blankDiv);  
+    }
+    deleteMe() {
+       let targetElement = document.getElementById(this.id);
+       this.iconDiv.addEventListener('click', () => {
+            projectArr.splice(this.id, 1);
+            localStorage.clear();
+            localStorage.setItem('ruleOne', JSON.stringify(projectArr));
+            targetElement.remove();
+            let targetDayList = document.getElementById(this.projectTitle);
+            targetDayList.remove();
+            
+       })
+    }
+};
+(function checkLocal(){
+    if(localStorage.length != 0) {
+        loadStorage = JSON.parse(localStorage.getItem("ruleOne"));
+        for(let i=0; i < loadStorage.length; i++) {
+            var elTitle = loadStorage[i].projectTitle;
+            var createNewProject = new Project(elTitle);
+            createNewProject.init();
+            projectArr.push(createNewProject);
+        }
+    }else {
+        console.log('Empty');
+    }
+})();
+function createNewProject() {
+    var title = prompt('Project Name');
+    const newProject = new Project(title);
+    newProject.init();
+    
+    projectArr.push(newProject);
+   
+    localStorage.setItem('ruleOne', JSON.stringify(projectArr));
+
+
+    makeNewDayList(title);
+};
+function makeNewDayList(title) {
+    let newDay = new Daylist(title);
+    newDay.startNewProject();
+    }
+
 
 //Time sandbar and animation controls
 function growSandBar(){
@@ -65,68 +322,3 @@ class Pixel {
         pixelArr[i].runAnimation();
     }
 })();
-//Project builder
-class Project {
-    constructor (projectTitle) {
-        this.projectTitle = projectTitle;
-        this.id = projectArr.length;
-    }
-    init() {
-        this.cacheDom();
-        this.buildProjectRectangle();
-        this.deleteMe();
-    }
-    cacheDom() {
-        this.projectPanel = document.getElementById('projectTabs');
-        this.blankDiv = document.createElement('div');
-        this.blankDiv.id = this.id;
-        this.titleDiv = document.createElement('div');
-        this.iconDiv = document.createElement('div');
-        this.iconDiv.className = 'gg-trash';
-        this.titleParaElement = document.createElement('p1');   
-    }
-    buildProjectRectangle() {
-        this.titleParaElement.innerText = this.projectTitle;
-        this.titleDiv.appendChild(this.titleParaElement);
-        this.blankDiv.appendChild(this.titleDiv);
-        this.blankDiv.appendChild(this.iconDiv);
-        this.projectPanel.appendChild(this.blankDiv);  
-    }
-    deleteMe() {
-       let targetElement = document.getElementById(this.id);
-       targetElement.addEventListener('click', () => {
-            projectArr.splice(this.id, 1);
-            localStorage.clear();
-            localStorage.setItem('ruleOne', JSON.stringify(projectArr))
-            targetElement.remove();
-
-       })
-    }
-}
-
-(function checkLocal(){
-    
-    if(localStorage.length != 0) {
-        loadStorage = JSON.parse(localStorage.getItem("ruleOne"));
-        for(let i=0; i < loadStorage.length; i++) {
-            var elTitle = loadStorage[i].projectTitle;
-            var createNewProject = new Project(elTitle);
-            createNewProject.init();
-            projectArr.push(createNewProject);
-        }
-        console.log(loadStorage);
-    }else {
-        console.log('Empty');
-    }
-})();
-
-function createNewProject() {
-    var title = prompt('Project Name');
-    const newProject = new Project(title);
-    projectArr.push(newProject);
-    newProject.init();
-    localStorage.setItem('ruleOne', JSON.stringify(projectArr));
-    console.log(projectArr);
-    console.log(localStorage);
-    
-}
